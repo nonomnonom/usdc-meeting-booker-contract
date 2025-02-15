ffcf3c85ecf4736f41024dfcd1b7b5e02289f5f2
nonom <nounsplayground@gmail.com>
2025-02-16 05:04:10 +0700

    Update route.ts

--- frame/app/.well-known/farcaster.json/route.ts
+++ frame/app/.well-known/farcaster.json/route.ts
@@ -4,7 +4,7 @@ export async function GET() {
   const config = {
     accountAssociation: {
       header: "eyJmaWQiOjEwMjAsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgxN2IzZmIzYTU4ZDc1RjFFMDRDNTFEQmVCMjdDNGEwOTQyRTk3MGE1In0",
-      payload: "",
+      payload: "eyJkb21haW4iOiJqYWtlLXBzaS52ZXJjZWwuYXBwIn0",
       signature: "MHgzZGNmOWRiNTg1ZjYzNzkyMTU4MjIzMThmOTBkZWM4OWMyYjhjOGZmMmI3NTI4NWUyMmM2NTM0NDRmODU2ZTg5MTE5MzMyYjIyZTNjYTNhNDdiNWIwNmVkZWM3ZDc2MWEzYjQ3ZjZmM2FkYzhiY2IxNjdlOWM5YmJmYmI5ZWU3YTFi"
     },
     frame: {
