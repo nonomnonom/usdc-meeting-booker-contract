c468e1952207893531f4d9eab7f16704c2c0a92a
nonomnonom <nounsplayground@gmail.com>
2025-02-16 04:50:38 +0700

    changes

--- frame/app/.well-known/farcaster.json/route.ts
+++ frame/app/.well-known/farcaster.json/route.ts
@@ -4,13 +4,13 @@ export async function GET() {
   const config = {
     accountAssociation: {
       header: "eyJmaWQiOjEwMjAsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgxN2IzZmIzYTU4ZDc1RjFFMDRDNTFEQmVCMjdDNGEwOTQyRTk3MGE1In0",
-      payload: "eyJkb21haW4iOiJsaWZlYWR2aWNlLXNpeC52ZXJjZWwuYXBwIn0",
+      payload: "",
       signature: "MHgzZGNmOWRiNTg1ZjYzNzkyMTU4MjIzMThmOTBkZWM4OWMyYjhjOGZmMmI3NTI4NWUyMmM2NTM0NDRmODU2ZTg5MTE5MzMyYjIyZTNjYTNhNDdiNWIwNmVkZWM3ZDc2MWEzYjQ3ZjZmM2FkYzhiY2IxNjdlOWM5YmJmYmI5ZWU3YTFi"
     },
     frame: {
       version: "1",
-      name: "Life Advice Booking",
-      iconUrl: `${appUrl}/icon.jpg`,
+      name: "Life Advice",
+      iconUrl: `${appUrl}/icon.png`,
       splashImageUrl: `${appUrl}/splash.png`,
       splashBackgroundColor: "#fafaf9",
       homeUrl: appUrl,
