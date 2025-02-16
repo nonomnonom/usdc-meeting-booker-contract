2d3d3cfac81017a56a12200fc87f5738a2438e23
nonom <nounsplayground@gmail.com>
2025-02-16 20:01:47 +0700

    Update WagmiProvider.tsx

--- frame/components/providers/WagmiProvider.tsx
+++ frame/components/providers/WagmiProvider.tsx
@@ -1,12 +1,12 @@
 import { createConfig, http, WagmiProvider } from "wagmi";
-import { baseSepolia } from "wagmi/chains";
+import { base } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { frameConnector } from "@/lib/connector";
 
 export const config = createConfig({
-  chains: [baseSepolia],
+  chains: [base],
   transports: {
-    [baseSepolia.id]: http(),
+    [base.id]: http(),
   },
   connectors: [frameConnector()],
 });
