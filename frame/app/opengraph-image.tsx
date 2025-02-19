80a4d87a2cd2a16948b87bd2f711596cb5a29860
nonom <nounsplayground@gmail.com>
2025-02-19 10:13:53 +0700

    Update opengraph-image.tsx

--- frame/app/opengraph-image.tsx
+++ frame/app/opengraph-image.tsx
@@ -1,4 +1,4 @@
-import { ImageResponse } from "next/og" // Import from next/og instead of next/server [^1]
+import { ImageResponse } from "next/og"
 
 export const alt = "Life Advice"
 export const size = {
@@ -8,40 +8,55 @@ export const size = {
 export const contentType = "image/png"
 
 export default async function Image() {
+  const interBlack = await fetch(
+    new URL('https://fonts.googleapis.com/css2?family=Inter:wght@900&display=swap', import.meta.url)
+  ).then((res) => res.arrayBuffer())
+
   return new ImageResponse(
-    <div
-      style={{
-        background: "white",
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: "40px",
-      }}
-    >
+    (
       <div
         style={{
-          width: "180px",
-          height: "180px",
-          borderRadius: "50%",
-          background: "blue",
-        }}
-      />
-      <div
-        style={{
-          fontSize: 72,
-          fontWeight: 900,
-          color: "#14171F",
-          letterSpacing: "-0.025em",
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "40px",
+          fontFamily: '"Inter"',
         }}
       >
-        <strong>LIFE ADVICE</strong>
+        <div
+          style={{
+            width: "180px",
+            height: "180px",
+            borderRadius: "50%",
+            background: "blue",
+          }}
+        />
+        <div
+          style={{
+            fontSize: 72,
+            fontWeight: 900,
+            color: "#000000",
+            letterSpacing: "-0.025em",
+          }}
+        >
+          LIFE ADVICE
+        </div>
       </div>
-    </div>,
+    ),
     {
       ...size,
-    },
+      fonts: [
+        {
+          name: 'Inter',
+          data: interBlack,
+          style: 'normal',
+          weight: 900
+        }
+      ]
+    }
   )
 }
