e59646e0241d519306d976c6f7106ee43d2b038f
nonom <nounsplayground@gmail.com>
2025-02-19 10:20:31 +0700

    Update opengraph-image.tsx

--- frame/app/opengraph-image.tsx
+++ frame/app/opengraph-image.tsx
@@ -1,62 +1,24 @@
-import { ImageResponse } from "next/og"
+import { ImageResponse } from "next/og";
 
-export const alt = "Life Advice"
+export const alt = "Life Advice";
 export const size = {
   width: 1200,
   height: 630,
-}
-export const contentType = "image/png"
+};
+export const contentType = "image/png";
 
 export default async function Image() {
-  const interBlack = await fetch(
-    new URL('https://fonts.googleapis.com/css2?family=Inter:wght@900&display=swap', import.meta.url)
-  ).then((res) => res.arrayBuffer())
-
   return new ImageResponse(
     (
-      <div
-        style={{
-          background: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "40px",
-          fontFamily: '"Inter"',
-        }}
-      >
-        <div
-          style={{
-            width: "180px",
-            height: "180px",
-            borderRadius: "50%",
-            background: "blue",
-          }}
-        />
-        <div
-          style={{
-            fontSize: 72,
-            fontWeight: 900,
-            color: "#000000",
-            letterSpacing: "-0.025em",
-          }}
-        >
+      <div tw="h-full w-full flex flex-col justify-center items-center bg-white">
+        <div tw="w-[180px] h-[180px] rounded-full bg-blue-500" />
+        <h1 tw="text-[72px] font-black text-[#14171F] tracking-tight mt-10">
           LIFE ADVICE
-        </div>
+        </h1>
       </div>
     ),
     {
       ...size,
-      fonts: [
-        {
-          name: 'Inter',
-          data: interBlack,
-          style: 'normal',
-          weight: 900
-        }
-      ]
     }
-  )
+  );
 }
